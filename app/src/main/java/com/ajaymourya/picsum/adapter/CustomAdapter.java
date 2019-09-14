fd4f2b0370133b68d592c3591d29f804c4887b12
Ajay Mourya <ajay@triveous.com>
2019-09-14 06:29:28 +0530

    created custom adapter for recycler view

---
+++ app/src/main/java/com/ajaymourya/picsum/adapter/CustomAdapter.java
@@ -0,0 +1,72 @@
+package com.ajaymourya.picsum.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ajaymourya.picsum.R;
+import com.ajaymourya.picsum.model.PhotoPojo;
+import com.jakewharton.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
+
+    private List<PhotoPojo> dataList;
+    private Context context;
+
+    public CustomAdapter(Context context, List<PhotoPojo> dataList) {
+        this.context = context;
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.list_item, parent, false);
+        return new CustomViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+        holder.fileName.setText(dataList.get(position).getFilename());
+        holder.authorName.setText(dataList.get(position).getAuthor());
+
+        Picasso.Builder builder = new Picasso.Builder(context);
+        builder.downloader(new OkHttp3Downloader(context));
+        builder.build().load(dataList.get(position).getPostUrl()).placeholder(R.drawable.ic_launcher_background).error(R.drawable.ic_launcher_background).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    class CustomViewHolder extends RecyclerView.ViewHolder {
+
+        public final View view;
+
+        TextView fileName;
+        TextView authorName;
+        ImageView imageView;
+
+        CustomViewHolder(View itemView) {
+            super(itemView);
+            view = itemView;
+
+            fileName = view.findViewById(R.id.file_name);
+            authorName = view.findViewById(R.id.author_name);
+            imageView = view.findViewById(R.id.image_view);
+        }
+    }
+
+
+}
