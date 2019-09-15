419c97d7f2d6106ad4798a467f844daa185e4f9e
Ajay Mourya <ajay@triveous.com>
2019-09-15 10:34:55 +0530

    moved image background download service to separate class

--- app/src/main/java/com/ajaymourya/picsum/adapter/CustomAdapter.java
+++ app/src/main/java/com/ajaymourya/picsum/adapter/CustomAdapter.java
@@ -1,14 +1,6 @@
 package com.ajaymourya.picsum.adapter;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
-import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,26 +9,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import androidx.core.app.TaskStackBuilder;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ajaymourya.picsum.R;
-import com.ajaymourya.picsum.activity.MainActivity;
 import com.ajaymourya.picsum.model.PhotoPojo;
+import com.ajaymourya.picsum.service.DownloadImage;
 import com.jakewharton.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Date;
 import java.util.List;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
@@ -129,135 +111,6 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
                     new DownloadImage(dataList.get(getAdapterPosition()).getFilename(), context).execute(dataList.get(getAdapterPosition()).getPostUrl());
                 }
             });
-        }
-    }
-
-    private static class DownloadImage extends AsyncTask<String, Void, Bitmap> {
-        private String TAG = "DownloadImage";
-        private String imageName;
-        private Context context;
-
-        private NotificationManagerCompat notificationManager;
-        private NotificationCompat.Builder builder;
-        private NotificationChannel mChannel;
-
-        int m = (int) ((new Date().getTime() / 1000L) % Integer.MAX_VALUE);
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            notificationManager = NotificationManagerCompat.from(context);
-
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                mChannel = new NotificationChannel(
-                        "id", "Channel Name", NotificationManager.IMPORTANCE_HIGH);
-                notificationManager.createNotificationChannel(mChannel);
-            }
-
-            builder = new NotificationCompat.Builder(context, "id");
-            builder.setContentTitle(imageName)
-                    .setContentText("Downloading Image")
-                    .setSmallIcon(R.drawable.ic_launcher_background)
-                    .setOnlyAlertOnce(true)
-                    .setPriority(NotificationCompat.PRIORITY_HIGH);
-
-            // Issue the initial notification with zero progress
-            int PROGRESS_MAX = 100;
-            int PROGRESS_CURRENT = 0;
-            builder.setProgress(PROGRESS_MAX, PROGRESS_CURRENT, false);
-
-        }
-
-        private Bitmap downloadImageBitmap(String sUrl) {
-            Bitmap bitmap = null;
-
-            int count;
-            try {
-                URL url = new URL(sUrl);
-                URLConnection urlConnection = url.openConnection();
-                urlConnection.connect();
-                int lenghtOfFile = urlConnection.getContentLength();
-                Log.e("file_length", " " + lenghtOfFile);
-                InputStream inputStream = new BufferedInputStream(url.openStream(), 8192);
-
-                byte data[] = new byte[1024];
-
-                long total = 0;
-
-                while ((count = inputStream.read(data)) != -1) {
-                    total += count;
-                    // publishing the progress....
-                    // After this onProgressUpdate will be called
-                    publishProgress("" + (int) ((total * 100) / lenghtOfFile));
-
-                }
-
-                bitmap = BitmapFactory.decodeStream(inputStream);
-                inputStream.close();
-            } catch (Exception e) {
-                Log.d(TAG, "Exception 1, Something went wrong!");
-                e.printStackTrace();
-            }
-            return bitmap;
-        }
-
-        private void publishProgress(String s) {
-
-            updateNotification(Integer.parseInt(s));
-        }
-
-        public DownloadImage(String imageName, Context context) {
-            this.imageName = imageName;
-            this.context = context;
-        }
-
-        @Override
-        protected Bitmap doInBackground(String... params) {
-
-            return downloadImageBitmap(params[0]);
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            if (result != null)
-                createDirectoryAndSaveFile(result, imageName);
-        }
-
-        private void updateNotification(int currentProgress) {
-
-            builder.setProgress(100, currentProgress, false);
-            if (currentProgress == 100)
-                builder.setContentText("Download Complete");
-            else
-                builder.setContentText("Downloaded: " + currentProgress + "%");
-            notificationManager.notify(m, builder.build());
-        }
-    }
-
-
-    private static void createDirectoryAndSaveFile(Bitmap imageToSave, String fileName) {
-
-        File direct = new File(Environment.getExternalStorageDirectory() + "/Picsum");
-
-        if (!direct.exists()) {
-            File wallpaperDirectory = new File(Environment.getExternalStorageDirectory() + "/Picsum");
-            wallpaperDirectory.mkdirs();
-
-        }
-
-        File file = new File(new File(Environment.getExternalStorageDirectory() + "/Picsum"), fileName);
-        if (file.exists()) {
-            file.delete();
-
-        }
-        try {
-            FileOutputStream out = new FileOutputStream(file);
-            imageToSave.compress(Bitmap.CompressFormat.JPEG, 100, out);
-            out.flush();
-            out.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
--- app/src/main/java/com/ajaymourya/picsum/service/DownloadImage.java
+++ app/src/main/java/com/ajaymourya/picsum/service/DownloadImage.java
@@ -1,4 +1,153 @@
 package com.ajaymourya.picsum.service;
 
-public class DownloadImage {
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.ajaymourya.picsum.R;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Date;
+
+public class DownloadImage extends AsyncTask<String, Void, Bitmap> {
+
+    private String TAG = "DownloadImage";
+    private String imageName;
+    private Context context;
+
+    private NotificationManagerCompat notificationManager;
+    private NotificationCompat.Builder builder;
+    private NotificationChannel mChannel;
+
+    int m = (int) ((new Date().getTime() / 1000L) % Integer.MAX_VALUE);
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+
+        notificationManager = NotificationManagerCompat.from(context);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            mChannel = new NotificationChannel(
+                    "id", "Channel Name", NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        builder = new NotificationCompat.Builder(context, "id");
+        builder.setContentTitle(imageName)
+                .setContentText("Downloading Image")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setOnlyAlertOnce(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        // Issue the initial notification with zero progress
+        int PROGRESS_MAX = 100;
+        int PROGRESS_CURRENT = 0;
+        builder.setProgress(PROGRESS_MAX, PROGRESS_CURRENT, false);
+
+    }
+
+    private Bitmap downloadImageBitmap(String sUrl) {
+        Bitmap bitmap = null;
+
+        int count;
+        try {
+            URL url = new URL(sUrl);
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.connect();
+            int lenghtOfFile = urlConnection.getContentLength();
+            Log.e("file_length", " " + lenghtOfFile);
+            InputStream inputStream = new BufferedInputStream(url.openStream(), 8192);
+
+            byte data[] = new byte[1024];
+
+            long total = 0;
+
+            while ((count = inputStream.read(data)) != -1) {
+                total += count;
+                // publishing the progress....
+                // After this onProgressUpdate will be called
+                publishProgress("" + (int) ((total * 100) / lenghtOfFile));
+
+            }
+
+            bitmap = BitmapFactory.decodeStream(inputStream);
+            inputStream.close();
+        } catch (Exception e) {
+            Log.d(TAG, "Exception 1, Something went wrong!");
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+    private void publishProgress(String s) {
+
+        updateNotification(Integer.parseInt(s));
+    }
+
+    public DownloadImage(String imageName, Context context) {
+        this.imageName = imageName;
+        this.context = context;
+    }
+
+    @Override
+    protected Bitmap doInBackground(String... params) {
+
+        return downloadImageBitmap(params[0]);
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        if (result != null)
+            createDirectoryAndSaveFile(result, imageName);
+    }
+
+    private void updateNotification(int currentProgress) {
+
+        builder.setProgress(100, currentProgress, false);
+        if (currentProgress == 100)
+            builder.setContentText("Download Complete");
+        else
+            builder.setContentText("Downloaded: " + currentProgress + "%");
+        notificationManager.notify(m, builder.build());
+    }
+
+    private void createDirectoryAndSaveFile(Bitmap imageToSave, String fileName) {
+
+        File direct = new File(Environment.getExternalStorageDirectory() + "/Picsum");
+
+        if (!direct.exists()) {
+            File wallpaperDirectory = new File(Environment.getExternalStorageDirectory() + "/Picsum");
+            wallpaperDirectory.mkdirs();
+
+        }
+
+        File file = new File(new File(Environment.getExternalStorageDirectory() + "/Picsum"), fileName);
+        if (file.exists()) {
+            file.delete();
+
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            imageToSave.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
